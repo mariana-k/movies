d7e3311ead1b9f2f625b6ef0ed38abb69d834142
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2025-07-15 14:09:58 +0300

    Movie details page props

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -3,21 +3,25 @@ import { HydrationBoundary } from '@tanstack/react-query'
 import QueryProvider from '../../QueryProvider'
 import MovieDetails from '@/components/MovieDetails'
 import { fetchMovieDetails } from '../../../api/tmdb'
-
-export default async function MovieDetailsPage({ params }: { params: { id: string } }) {
-  const id = Number(params.id)
+interface PageProps {
+  params: { id: string }
+}
+const Page: React.FC<PageProps> = async ({ params }) => {
+  const { id } = await params
+  const numberId = Number(id)
   const queryClient = createSSRQueryClient()
   await queryClient.prefetchQuery({
-    queryKey: ['movie-details', id],
-    queryFn: () => fetchMovieDetails(id),
+    queryKey: ['movie-details', numberId],
+    queryFn: () => fetchMovieDetails(numberId),
   })
   const dehydratedState = dehydrateState(queryClient)
 
   return (
     <QueryProvider>
       <HydrationBoundary state={dehydratedState}>
-        <MovieDetails id={id} />
+        <MovieDetails id={numberId} />
       </HydrationBoundary>
     </QueryProvider>
   )
 }
+export default Page
